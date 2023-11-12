9e2fd81b95c1f53a1bdff59cde4d82be1171f579
amoz <amoz_c.p.t@hotmail.com>
2023-11-12 13:23:59 -0300

    Upadate Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_home_service/src/pages/account/account_page.dart';
 import 'package:projeto_home_service/src/pages/home/home_page.dart';
 import 'package:projeto_home_service/src/pages/login/login_page.dart';
 import 'package:projeto_home_service/src/pages/main/main_page.dart';
 import 'package:projeto_home_service/src/pages/onboarding/onboard_controller.dart';
 import 'package:projeto_home_service/src/pages/registry/registry_page.dart';
+import 'package:projeto_home_service/src/pages/search/search_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +28,9 @@ class MyApp extends StatelessWidget {
         '/registry': (context) => const RegistryPage(),
         '/main_page': (context) => const MainPage(),
         '/login': (context) => const LoginPage(),
+        '/home_page': (context) => const HomePage(),
+        '/search_page': (context) => const SearchPage(),
+        '/account_page': (context) => const AccountPage(),
       },
     );
   }
