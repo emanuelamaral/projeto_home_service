5145493610098273a41565e41c4ade12df47e5f7
emanuelamaral <amoz_c.p.t@hotmail.com>
2023-12-14 04:09:17 -0300

    Update HomePage

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -2,25 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:projeto_home_service/src/components/homePage/combos_services_card.dart';
 import 'package:projeto_home_service/src/components/homePage/home_services_card_list.dart';
 import 'package:projeto_home_service/src/components/homePage/popular_home_services_card.dart';
+import 'package:projeto_home_service/src/provider/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    final user = Provider.of<UserProvider>(context).user;
+    return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 120),
-            CombosServicesCard(),
-            SizedBox(height: 20),
-            HomeServiceCardList(),
-            SizedBox(height: 20),
-            PopularHomeServicesCard(),
+            const SizedBox(height: 60),
+            const Text(
+              'Bem vindo de volta!',
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  // fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+            Text(
+              '${user.name}',
+              style: const TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            const CombosServicesCard(),
+            const SizedBox(height: 20),
+            const HomeServiceCardList(),
+            const SizedBox(height: 20),
+            const PopularHomeServicesCard(),
           ],
         ),
       ),
